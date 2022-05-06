335866328c3c56f8d7fa5f69f50082d96d93c6f4
Igor Neifach <igor.neifach@teampicnic.com>
2022-05-07 01:09:31 +0200

    Added functionality to execute queries in the DWH.
     - Installed picnic-database
     - Added deliveries.sql file containing query
     - Added ._extract method to the handler

--- practice_dwh/handler.py
+++ practice_dwh/handler.py
@@ -1,8 +1,15 @@
 """High level logic handler."""
 
+from asyncio.log import logger
 import logging
 
+from picnic.database import DatabaseClientFactory
+from picnic.definitions import Environment
+
+from practice_dwh import PROJECT_ROOT_DIR
+
 LOGGER = logging.getLogger(__name__)
+QUERY_PATH = PROJECT_ROOT_DIR / "queries"
 
 
 class PracticeDwhHandler:
@@ -20,8 +27,21 @@ class PracticeDwhHandler:
         self.name = name
 
     def run(self):
-        """Log the name set by the initializer.
-
-        This method plays a role as an entrypoint to the business logic.
+        """Execute ETL to get the total orders (to be) delivered yesterday and today.
         """
-        LOGGER.info(self.name)
+        self._extract()
+
+    def _extract(self):
+        dwh_client = DatabaseClientFactory(
+            Environment("local"),
+            database="picnic_nl_prod", 
+            warehouse="ANALYSIS",
+            role="analyst",
+            account="uj82639.eu-west-1",
+            username="igor.neifach@teampicnic.com",
+        ).get_client()
+        orders_yesterday = next(
+            dwh_client.select(query=(QUERY_PATH/"deliveries.sql").read_text()).as_dicts()
+        )["delivered_orders"]
+
+        LOGGER.info(f"Nr. of fulfilled orders yesterday: {orders_yesterday}")
